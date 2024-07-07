375321d2ba3632159510ec4d54fb79e60a0f4ba2
Nicolas <nicolasecrespo@gmail.com>
2024-07-06 21:29:02 -0400

    ???

--- js/src/script.js
+++ js/src/script.js
@@ -1,8 +1,12 @@
 // comment
 document.getElementById("button").addEventListener("click", function () {
   alert("there was a cluck");
+  console.log("there was a cluck");
 });
 
-// document.getElementById("button").addEventListener("mouseenter", function () {
-//   alert("there was a cluck");
-// });
+//while cursor is on button, console log hello
+
+var count = 0;
+document.getElementById("button").addEventListener("mouseenter", function () {
+  console.log("hello", count++);
+});
