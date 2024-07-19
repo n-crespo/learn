c5a27b681724f1e68eb2079c6b8b9c6a430fadcb
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-07-18 20:16:34 -0400

    indent

--- py/test.py
+++ py/test.py
@@ -1,29 +1,22 @@
 from pynput import keyboard
-import threading
 import time
 
-space_pressed = False
-
-
 def on_press(key):
-    global space_pressed
     if key == keyboard.Key.space:
-        space_pressed = True
         return False  # Stop listener
 
-
 def wait_for_space(timeout):
-    global space_pressed
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
 
     start_time = time.time()
-    while not space_pressed and (time.time() - start_time) < timeout:
+    while (time.time() - start_time) < timeout:
+        if not listener.running:
+            return True
         time.sleep(0.1)  # Sleep a bit to avoid busy waiting
 
     listener.stop()
-    return space_pressed
-
+    return False
 
 def message(prompt, options):
     print(prompt)
@@ -35,5 +28,5 @@ def message(prompt, options):
     else:
         print("Timeout: You did not press the space bar within 4 seconds.")
 
-
 message("Please select 1 or 2", ["1", "2"])
+
